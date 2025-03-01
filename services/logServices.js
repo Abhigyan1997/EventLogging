b94382d3cf51ae706a4b05bb9e8d9a9fec69eeb8
Abhigyan1997 <alokabhigyan65@gmail.com>
2025-03-01 11:44:52 +0530

    Practice git

--- services/logServices.js
+++ services/logServices.js
@@ -21,7 +21,7 @@ const getLogs = async (filters, pagination) => {
   const logs = await Log.find(filters).sort({ timestamp: -1 }).skip(skip).limit(limit);
   const totalCount = await Log.countDocuments(filters);
   return { logs, totalCount };
-  console.log(logs)
+  console.log(l)
 };
 
 
